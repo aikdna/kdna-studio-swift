b709b04102b52b9ceade3892ebd0301ac6b9e5e4
aikdna <9141164@qq.com>
2026-08-09 15:05:33 +0800

    chore: pin kdna-core-swift to the v0.21.0 release tag
    Adopt the published Swift Core 0.21.0 contract (tag v0.21.0 at 1e13ef5) in place of the pre-#30 raw revision 95f638e.
    
    Signed-off-by: aikdna <9141164@qq.com>

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/aikdna/kdna-core-swift.git",
-            revision: "95f638e2f0472a375704fb5fe2f057de0cb4cb07"
+            from: "0.21.0"
         ),
         .package(
             url: "https://github.com/aikdna/kdna-app-shared.git",
